c7e80a31c80396e38d585a9464a1e520b017eac5
OB11TO <OB11TO61@yandex.ru>
2021-12-19 21:47:44 +0300

    fix

--- src/section001_classes/DynaArray.java
+++ src/section001_classes/DynaArray.java
@@ -3,4 +3,13 @@ package section001_classes;
 public class DynaArray {
     int[] result = new int[5];
     int counter;
+
+    public void add( int val) {
+        if(counter == result.length){
+            int[] newArray = new int[result.length * 2];
+            System.arraycopy(result, 0, newArray, 0, result.length);
+            result = newArray;
+        }
+        result[counter++] = val;
+    }
 }
