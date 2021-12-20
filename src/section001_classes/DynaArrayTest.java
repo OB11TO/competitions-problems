4cb4731555fcf6e208a67692b94bdec8b6337466
OB11TO <OB11TO61@yandex.ru>
2021-12-21 01:15:42 +0300

    new test

--- src/section001_classes/DynaArrayTest.java
+++ src/section001_classes/DynaArrayTest.java
@@ -37,3 +37,28 @@ public class DynaArrayTest {
         System.out.println(dynaArray2.getCounter());
     }
 }
+class DynaArrayTest2 {
+    public static void main(String[] args) {
+        DynaArray dynaArray = new DynaArray(0);
+
+        dynaArray.add(0);
+        dynaArray.add(1);
+        dynaArray.add(2);
+        dynaArray.add(3);
+        dynaArray.add(4);
+        dynaArray.add(5);
+
+        dynaArray.add(new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1});
+
+        DynaArray dynaArray2 = new DynaArray(0);
+        dynaArray2.add(new int[]{-9, -8, -7, -6, -5, -4, -3, -2, -1, 0});
+        dynaArray.add(dynaArray2);
+
+        dynaArray.add(99);
+        dynaArray.add(98);
+        dynaArray.add(97);
+
+        // [0, 1, 2, 3, 4, 5, 9, 8, 7, 6, 5, 4, 3, 2, 1, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 99, 98, 97]
+        System.out.println(dynaArray.asString());
+    }
+}
