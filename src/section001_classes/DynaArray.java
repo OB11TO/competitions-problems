4641702950a2d34b0eb2b3e03323e6bdb15d8dd8
OB11TO <OB11TO61@yandex.ru>
2021-12-20 16:30:10 +0300

    add asString method

--- src/section001_classes/DynaArray.java
+++ src/section001_classes/DynaArray.java
@@ -6,8 +6,8 @@ public class DynaArray {
     int[] result = new int[5];
     int counter;
 
-    public void add( int val) {
-        if(counter == result.length){
+    public void add(int val) {
+        if (counter == result.length) {
             int[] newArray = new int[result.length * 2];
             System.arraycopy(result, 0, newArray, 0, result.length);
             result = newArray;
@@ -15,7 +15,26 @@ public class DynaArray {
         result[counter++] = val;
     }
 
-    public int[] toArray(){
-        return Arrays.copyOf(result,counter);
+    public int[] toArray() {
+        return Arrays.copyOf(result, counter);
+    }
+
+    public String asString() {
+        final StringBuilder sb = new StringBuilder().append('[');
+        for (int i = 0; i < counter; i++) {
+            sb.append(result[i]);
+            if(i < counter - 1){
+                sb.append(", ");
+            }
+        }
+     /*
+        if (sb.length() > 0) {
+            sb.deleteCharAt(sb.length() - 1);
+            sb.deleteCharAt(sb.length() - 1).append("]");
+        }
+
+      */
+
+        return sb.append("]").toString();
     }
 }
