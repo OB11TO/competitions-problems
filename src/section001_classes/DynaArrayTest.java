2a9b841ae280eaf65639d9ba3783026b0a3f3e36
OB11TO <OB11TO61@yandex.ru>
2021-12-21 23:39:37 +0300

    add contains test

--- src/section001_classes/DynaArrayTest.java
+++ src/section001_classes/DynaArrayTest.java
@@ -111,3 +111,30 @@ class DynaArrayTest5 {
         System.out.println(dynaArray.asString());
     }
 }
+
+class DynaArrayTest6 {
+    public static void main(String[] args) {
+        DynaArray dynaArray = new DynaArray();
+        dynaArray.add(0);
+        dynaArray.add(1);
+        dynaArray.add(2);
+        dynaArray.add(3);
+
+        System.out.println(dynaArray.size());
+    }
+}
+class DynaArrayTest7 {
+    public static void main(String[] args) {
+        DynaArray dynaArray = new DynaArray();
+        dynaArray.add(0);
+        dynaArray.add(1);
+        dynaArray.add(2);
+        dynaArray.add(3);
+
+        System.out.println(dynaArray.contains(0));
+        System.out.println(dynaArray.contains(2));
+
+        System.out.println(dynaArray.contains(100));
+        System.out.println(dynaArray.contains(-3));
+    }
+}
