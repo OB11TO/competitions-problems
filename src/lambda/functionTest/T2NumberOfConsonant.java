0ff4d1219f38b83fde06973219d2d4d4ad1a36bb
OB11TO <OB11TO61@yandex.ru>
2022-03-17 19:11:06 +0300

    add task stepik

---
+++ src/lambda/functionTest/T2NumberOfConsonant.java
@@ -0,0 +1,11 @@
+package lambda.functionTest;
+
+
+/**
+ * Опишите такую реализацию Function<String, Integer> которая вернет количество согласных букв в
+ * строке текста.
+ */
+public class T2NumberOfConsonant {
+
+
+}
