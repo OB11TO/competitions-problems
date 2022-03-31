2e5d50525fd22eee981526081826ee6c0d268a58
OB11TO <OB11TO61@yandex.ru>
2022-03-31 14:24:55 +0300

    lambda UnaryOperator HW1

--- src/lambda/consumerTest/T1.java
+++ src/lambda/consumerTest/T1.java
@@ -8,7 +8,7 @@ import java.util.function.Consumer;
  * 1)Реализуйте Consumer<Integer> с помощью которого выведите на экран все нечетные
  * числа содержащиеся в List<Integer>.
  */
-public class T1 {
+public class T1 implements A, B {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>(List.of(1,2,3,4,5,6,7,8,9));
 
@@ -20,6 +20,25 @@ public class T1 {
 
         list.forEach(result);
     }
+
+    @Override
+    public int sum() {
+        return 0;
+    }
+
 }
+
+
+interface A{
+    int sum();
+}
+
+interface B{
+    int sum();
+}
+
+
+
+
 
 
