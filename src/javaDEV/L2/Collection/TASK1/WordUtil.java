ea5c37f039bdc5dc87cf254d2f69ffbf00c9c4f5
OB11TO <OB11TO61@yandex.ru>
2022-04-05 18:05:51 +0300

    dmdev collection HW TASK 1

---
+++ src/javaDEV/L2/Collection/TASK1/WordUtil.java
@@ -0,0 +1,29 @@
+package javaDEV.L2.Collection.TASK1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordUtil {
+    private static final String EMPTY = "";
+    private static final String SPACE = " ";
+
+    private WordUtil() {
+    }
+
+    public static Map<String, Integer> calcWordFrequency(String text){
+        Map<String, Integer> resultMap = new HashMap<>();
+        String[] words = text
+                .replace(".", EMPTY)
+                .replace(",", EMPTY)
+                .replace("!", EMPTY)
+                .split(SPACE);
+
+        for(String word : words){
+            Integer oldGet = resultMap.get(word);
+            Integer newGet = oldGet == null ? 1 : oldGet+1;
+            resultMap.put(word, newGet);
+        }
+
+        return resultMap;
+    }
+}
