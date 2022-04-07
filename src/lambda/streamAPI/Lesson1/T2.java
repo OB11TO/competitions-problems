45fae53500b1b242fd779847235f5bd6312f723b
OB11TO <OB11TO61@yandex.ru>
2022-04-07 15:14:07 +0300

    lambda stream LESSON 1 task 2

---
+++ src/lambda/streamAPI/Lesson1/T2.java
@@ -0,0 +1,22 @@
+package lambda.streamAPI.Lesson1;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 2)Используя Stream API и List<Integer> выделите только нечетные числа, отсортируйте их
+ * по возрастанию и соберите в новый список.
+ */
+
+public class T2 {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(-1,4,-3,2,6,11,8,7,9);
+
+        List<Integer> collect = list.stream()
+                .filter(integer -> integer % 2 != 0)
+                .sorted()
+                .collect(Collectors.toList());
+
+        System.out.println(collect);
+    }
+}
