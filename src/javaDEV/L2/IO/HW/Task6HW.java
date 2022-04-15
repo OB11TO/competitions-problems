d018442b9f3aa70fe385d4c3ca81e049a4f512b4
OB11TO <OB11TO61@yandex.ru>
2022-04-15 13:46:50 +0300

    javaDEV IO HW2

---
+++ src/javaDEV/L2/IO/HW/Task6HW.java
@@ -0,0 +1,65 @@
+package javaDEV.L2.IO.HW;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Task6HW {
+    public static void main(String[] args) throws IOException {
+        Path pathInput = Path.of("src", "javaDEV", "L2", "IO", "resources", "test.txt");
+
+        Map<Character, Integer> characterCountMap = new HashMap<>();
+
+        List<String> listLines = Files.readAllLines(pathInput);
+
+      listLines.stream()
+                .map(Task6HW::characterSearchCountMap)
+                .forEach(System.out::println);
+
+      /*  for(Map<Character, Integer> map : mapList){
+            Set<Map.Entry<Character, Integer>> entrySet = map.entrySet();
+            for (Iterator<Map.Entry<Character, Integer>> iterator = entrySet.iterator(); iterator.hasNext();){
+                Map.Entry<Character, Integer> entry = iterator
+                        .next();
+                System.out.println(entry.getKey() + " relative frequency ="
+                        + ((float) entry.getValue()) / text.length());
+            }
+        }*/
+
+
+      /*  Set<Entry<Character, Integer>> entrySet = characterCountMap.entrySet();
+
+        for (Iterator iterator = entrySet.iterator(); iterator.hasNext();)
+        {
+            Entry<Character, Integer> entry = (Entry<Character, Integer>) iterator
+                    .next();
+            System.out.println(entry.getKey() + " relative frequency ="
+                    + ((float) entry.getValue()) / text.length());
+
+        }*/
+
+
+    }
+
+    private static  Map<Character,Integer> characterSearchCountMap(String line) {
+        Map<Character,Integer> result = new HashMap<>();
+
+        for(int i = 0; i < line.length(); i++){
+            char c = line.charAt(i);
+            result.merge(c, 1, Integer::sum);
+
+        }
+
+            Set<Map.Entry<Character, Integer>> entrySet = result.entrySet();
+        for (Map.Entry<Character, Integer> entry : entrySet) {
+            /*System.out.println(entry.getKey() + " relative frequency ="
+                    + ((float) entry.getValue()) / line.length());*/
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
+
+        return  result;
+    }
+}
---
+++ src/javaDEV/L2/IO/HW/task2/Task2Runner.java
@@ -0,0 +1,13 @@
+package javaDEV.L2.IO.HW.task2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Task2Runner {
+    public static void main(String[] args) throws IOException {
+        Path pathOutput = Path.of("src", "javaDEV", "L2", "IO", "resources", "task2.txt");
+        Files.write(pathOutput, RandomUtil.countRandom(100));
+        Files.write(pathOutput, DigitHelper.readSorted(pathOutput));
+    }
+}
