bc66e30fddc85305393fa1262d29d279727f59b4
OB11TO <OB11TO61@yandex.ru>
2022-04-15 19:51:01 +0300

    javaDEV DATE PRAC Test

---
+++ src/javaDEV/L2/Date/DateDemo.java
@@ -0,0 +1,10 @@
+package javaDEV.L2.Date;
+
+import java.time.ZonedDateTime;
+
+public class DateDemo {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
