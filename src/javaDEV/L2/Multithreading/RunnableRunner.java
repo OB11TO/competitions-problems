26812bc9835b7b17ee69d31da9e0aece07e1b2fc
OB11TO <OB11TO61@yandex.ru>
2022-04-20 17:18:05 +0300

    javaDEV MULT TASK

---
+++ src/javaDEV/L2/Multithreading/RunnableRunner.java
@@ -0,0 +1,8 @@
+package javaDEV.L2.Multithreading;
+
+public class RunnableRunner implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Runnable " + Thread.currentThread().getName());
+    }
+}
