50aa723dcf758ecb5fcef7bed77e4f081635b0d6
OB11TO <OB11TO61@yandex.ru>
2022-04-22 16:37:14 +0300

    javaDEV MULT DeadLock

---
+++ src/javaDEV/L2/Multithreading/DeadLock/Account.java
@@ -0,0 +1,32 @@
+package javaDEV.L2.Multithreading.DeadLock;
+
+public class Account {
+    private static int generator = 1;
+    private int id;
+    private int money;
+
+    public Account(int money) {
+        this.id = generator++;
+        this.money = money;
+    }
+
+    public void add(int money){
+        this.money += money;
+    }
+
+    public boolean takeOff(int money){
+        if(this.money > money){
+            this.money -= money;
+            return true;
+        }
+        return false;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Account{" + "id=" + id +
+                ", money=" + money +
+                '}';
+    }
+}
