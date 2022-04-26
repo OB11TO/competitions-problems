29a2199f57e35e79652d98a07d6e4756517c7238
OB11TO <OB11TO61@yandex.ru>
2022-04-26 18:28:39 +0300

    javaDEV SQL Standard T2

--- src/SQL/T/standart.sql
+++ src/SQL/T/standart.sql
@@ -5,7 +5,7 @@ DROP SCHEMA company_storage;
 
 
 
-CREATE TABLE company_storage.company
+CREATE TABLE company
 (
     id   INT PRIMARY KEY,
     name VARCHAR(128) NOT NULL UNIQUE,
@@ -169,36 +169,28 @@ values ('Ivan', 'Ivanov', 1500, 1),
 --UNION - обрезает дублирующие значения
 --UNION ALL - выводит всё
 
-SELECT
-    first_name
+SELECT first_name
 FROM employee
 WHERE company_id IS NOT NULL
 UNION ALL
-SELECT
-    first_name
+SELECT first_name
 FROM employee
 WHERE salary IS NULL;
 
 
-
-
-
 --------------------------------------------------ПОДЗАПРОСЫ 13-----------------------------------------------------
 
-SELECT
-    sum(salary)
-FROM (SELECT
-          *
+SELECT sum(salary)
+FROM (SELECT *
       FROM employee
       ORDER BY salary
-          LIMIT 2) empl;
+      LIMIT 2) empl;
 
 
-SELECT
-    *
+SELECT *
 FROM employee
 ORDER BY salary
-    LIMIT 2;
+LIMIT 2;
 
 
 SELECT *,
@@ -214,7 +206,8 @@ FROM employee;
 
 --DELETE FROM employee; -- удалит все данные в таблице (НЕбЕЗОПАСНЫЙ ЗАПРОС)
 
-select * from employee;
+select *
+from employee;
 
 DELETE
 FROM employee
@@ -224,3 +217,183 @@ DELETE
 FROM employee
 WHERE salary = (SELECT max(salary)
                 FROM employee);
+
+
+---------------------------------------------------------------------------------------------------------------------------
+CREATE DATABASE book_repository;
+
+
+-------------------------------------------INNER JOIN PR 20------------------------------------------------------------------------------
+CREATE TABLE company
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(128) NOT NULL
+);
+DROP TABLE employee;
+DROP TABLE company;
+CREATE TABLE employee
+(
+    id         SERIAL PRIMARY KEY,
+    first_name VARCHAR(128),
+    last_name  VARCHAR(128),
+    company_id INT REFERENCES company (id)
+);
+
+CREATE TABLE contact
+(
+    id     SERIAL PRIMARY KEY,
+    number VARCHAR(128) NOT NULL,
+    type   VARCHAR(128)
+);
+DROP TABLE employee_contact;
+DROP TABLE contact;
+
+CREATE TABLE employee_contact
+(
+    employee_id INT REFERENCES employee (id),
+    contact_id  INT REFERENCES contact (id),
+    PRIMARY KEY (employee_id, contact_id)
+);
+
+insert into contact (number, type)
+values ('234-56-78', 'домашний'),
+       ('987-65-43', 'рабочий'),
+       ('565-25-91', 'мобильный'),
+       ('332-55-67', NULL),
+       ('465-11-22', NULL);
+
+insert into employee (first_name, last_name, company_id)
+values ('Ivan', 'Ivanov', 1),
+       ('Sveta', 'Svetikova', 2),
+       ('Petr', 'Petrov', 3);
+
+insert into employee_contact (employee_id, contact_id)
+values (1, 1),
+       (1, 2),
+       (2, 2),
+       (2, 3),
+       (3, 4),
+       (4, 5);
+
+------------------------------------------ПРОДОЛЖЕНИЕ-------------------------------------------------------------------------------
+
+--INNER JOIN
+--CROSS JOIN
+--RIGHT JOIN
+--FULL JOIN
+
+SELECT company.name,
+       employee.first_name || ' ' || employee.last_name fio,
+       ec.contact_id,
+       c.number || ' ' || c.type
+FROM employee
+         JOIN company
+              ON company.id = employee.company_id
+         JOIN employee_contact ec
+              ON employee.id = ec.employee_id
+         JOIN contact c on c.id = ec.contact_id;
+
+SELECT *
+from company
+         cross join (select count(*) from employee) t1;
+
+
+select company.name,
+       e.first_name
+       -- count(*)
+from company
+         left join employee e
+                   on company.id = e.company_id
+where company.name = 'Apple';
+-- group by company.name  -- группировка
+-- having count(*) <2;   -- фильтрация
+
+
+--------------------------------------Оконные функции Windows function---------------------------------------------------
+
+select c.name,
+--        e.first_name,
+       count(*)
+from company c
+         left join employee e
+                   on c.id = e.company_id
+group by c.name
+
+order by c.name;
+
+
+
+--OVER() и есть оконная функция
+--partition by - ранжирование происходит внутри по группам
+-- lag(e.salary) over (order by e.salary) простое смещение
+select c.name,
+       e.first_name,
+       e.salary,
+       row_number() over (partition by first_name),                           -- одна из оконных функций
+       count(e.id) OVER (),
+       max(e.salary) OVER (),
+--         avg(e.salary) OVER(),
+       lag(e.salary) over (order by e.salary),
+       rank() OVER (order by e.salary nulls first ),                          -- nulls firs (null поля ставим первыми)
+       dense_rank() OVER (partition by c.name order by e.salary nulls first ) -- не пропускает места
+from company c
+         left join employee e
+                   on c.id = e.company_id;
+
+--------------------------------------Представление.View-----------------------------------------------------------------------
+
+--CREATE VIEW employee_view по сути оболочка, когда мы вызываем её вызывается SELECT
+--CREATE MATERIALIZED VIEW employee_materialized_view AS - отличие в том, что сохраняет запрос, но изменять данные нельзя
+--REFRESH MATERIALIZED VIEW - обновить данные в таблице кэша для MATERIALIZED
+
+CREATE VIEW employee_view AS
+select c.name,
+       e.first_name,
+       e.salary,
+       row_number() over (partition by first_name),                           -- одна из оконных функций
+       count(e.id) OVER (),
+       max(e.salary) OVER (),
+--         avg(e.salary) OVER(),
+       lag(e.salary) over (order by e.salary),
+       rank() OVER (order by e.salary nulls first ),                          -- nulls firs (null поля ставим первыми)
+       dense_rank() OVER (partition by c.name order by e.salary nulls first ) -- не пропускает места
+from company c
+         left join employee e
+                   on c.id = e.company_id;
+
+
+
+select *
+from employee_view
+where name = 'Facebook';
+
+CREATE MATERIALIZED VIEW employee_materialized_view AS
+select c.name,
+       e.first_name,
+       e.salary,
+       row_number() over (partition by first_name),                           -- одна из оконных функций
+       count(e.id) OVER (),
+       max(e.salary) OVER (),
+--         avg(e.salary) OVER(),
+       lag(e.salary) over (order by e.salary),
+       rank() OVER (order by e.salary nulls first ),                          -- nulls firs (null поля ставим первыми)
+       dense_rank() OVER (partition by c.name order by e.salary nulls first ) -- не пропускает места
+from company c
+         left join employee e
+                   on c.id = e.company_id;
+
+--------------------------------------Изменение таблиц. ALTER----------------------------------------------------------------------
+
+ALTER TABLE IF EXISTS employee
+    ADD COLUMN gender INT;
+
+update employee
+set gender = 1
+where id <= 3;
+
+select employee.gender
+from employee;
+
+--Удалить
+ALTER TABLE IF EXISTS employee
+    DROP COLUMN  gender;
