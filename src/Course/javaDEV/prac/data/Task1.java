0fedd90aa0da4028562798d6607091a078108521
OB11TO <OB11TO61@yandex.ru>
2022-07-21 10:47:25 +0300

    1

---
+++ src/Course/javaDEV/prac/data/Task1.java
@@ -0,0 +1,18 @@
+package Course.javaDEV.prac.data;
+
+
+import java.time.LocalDateTime;
+
+/**
+ * 1. Создать объект LocalDateTime, представляющий собой
+ * дату 25.06.2020 19:47. Используя этот объект, создать другой объект LocalDateTime,
+ * представляющий собой дату через 3 месяца после сегодняшней.
+ * Вывести на консоль содержащиеся в нем объекты LocalDate and LocalTime.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        LocalDateTime dateTime = LocalDateTime.of(2020,6,25,19,47);
+        var plusMonths = dateTime.plusMonths(3);
+
+    }
+}
