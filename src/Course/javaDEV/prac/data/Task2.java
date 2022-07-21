78551e03d91bb00dc5cb7cdcb889feaaeb3f4a4e
OB11TO <OB11TO61@yandex.ru>
2022-07-21 10:47:40 +0300

    Data - prac

---
+++ src/Course/javaDEV/prac/data/Task2.java
@@ -0,0 +1,20 @@
+package Course.javaDEV.prac.data;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.zip.DataFormatException;
+
+/**
+ * Создать объект LocalDate, представляющий собой сегодняшнюю
+ * дату. Преобразовать дату в строку вида "05.05.2017". Вывести эту строку на
+ * консоль.
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        var now = LocalDate.now();
+
+        var dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        var format = now.format(dateTimeFormatter);
+        System.out.println(format);
+    }
+}
