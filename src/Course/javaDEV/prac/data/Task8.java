f2d1e09a46fd67fd7907a51e289e22b5d80e650a
OB11TO <OB11TO61@yandex.ru>
2022-07-22 11:17:45 +0300

    Data - prac

---
+++ src/Course/javaDEV/prac/data/Task8.java
@@ -0,0 +1,22 @@
+package Course.javaDEV.prac.data;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+
+/**
+ *  Написать свою реализацию интерфейса TemporalAdjuster, которая бы
+ * прибавляла к дате 42 дня
+ */
+public class Task8 {
+    public static void main(String[] args) {
+        TemporalAdjuster temporalAdjuster = temporal -> temporal.plus(42L, ChronoUnit.DAYS);
+
+        var now = LocalDate.now();
+        var result = now.with(temporalAdjuster);
+
+        System.out.println(now);
+        System.out.println(result);
+    }
+}
