6ee917c183ae1899fa4f923ad7539ecd0ca88cd2
OB11TO <OB11TO61@yandex.ru>
2022-07-22 11:17:40 +0300

    Data - prac

---
+++ src/Course/javaDEV/prac/data/Task7.java
@@ -0,0 +1,20 @@
+package Course.javaDEV.prac.data;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+/**
+ *  Создать объект Instant. Вывести его на консоль. Затем создать
+ * объект ZonedDateTime на основании предыдущего объекта с тайм зоной "Africa/Cairo"
+ */
+public class Task7 {
+    public static void main(String[] args) {
+
+        Instant instant = Instant.now();
+        System.out.println(instant);
+
+        var zonedDateTime = ZonedDateTime.ofInstant(instant, ZoneId.of("Africa/Cairo"));
+        System.out.println(zonedDateTime);
+    }
+}
