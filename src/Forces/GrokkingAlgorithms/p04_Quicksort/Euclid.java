35cd8ab4a8e8fdff7b634668dbd067e2e38fcdd2
OB11TO <OB11TO61@yandex.ru>
2022-11-17 13:01:14 +0300

    GrokkingAlgorithms - 3 - Euclid

--- src/Forces/GrokkingAlgorithms/p04_Quicksort/Euclid.java
+++ src/Forces/GrokkingAlgorithms/p04_Quicksort/Euclid.java
@@ -2,12 +2,13 @@ package Forces.GrokkingAlgorithms.p04_Quicksort;
 
 /**
  * Реализация алгоритма Евклида
+ * O (Log min (n1, n2)
  */
 public class Euclid {
 
     public static void main(String[] args) {
         System.out.println(searchEuclid(1680, 640)); //80
-        System.out.println(searchEuclid(168, 64)); //80
+        System.out.println(searchEuclid(400, 64)); //16
     }
 
     private static int searchEuclid(int first, int second) {
