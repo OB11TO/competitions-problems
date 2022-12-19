37d13098ed8401e750494bd469d15ba835908953
OB11TO <OB11TO61@yandex.ru>
2022-12-19 15:11:15 +0300

    [Multithreading] -> lesson 25.9-10 wait notifyAll queue

---
+++ src/course/multithreading/javacore/dmdev/lesson25_9/CashBox.java
@@ -0,0 +1,17 @@
+package course.multithreading.javacore.dmdev.lesson25_9;
+
+public class CashBox {
+
+    private static int generator = 1;
+    private int id;
+
+    public CashBox() {
+        this.id = generator++;
+    }
+
+    @Override
+    public String toString() {
+        return "CashBox{" + "id=" + id +
+                '}';
+    }
+}
