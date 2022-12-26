c998f1c41b2c1a0c8b23c1a3611723cddcac9e10
OB11TO <OB11TO61@yandex.ru>
2022-12-26 14:06:49 +0300

    [leetcode] -> 26. Remove Duplicates from Sorted Array

--- src/leetcode/easy/RemoveDuplicatesFromSortedArray.java
+++ src/leetcode/easy/RemoveDuplicatesFromSortedArray.java
@@ -13,6 +13,16 @@ public class RemoveDuplicatesFromSortedArray {
         }
     }
 
+    /*
+    Алгоритм:
+    Сортировка гарантированна
+    Два указателя - insertNums / i
+    [1,1,2,3,4,5]
+    insertNums = 1;
+    i = 1;
+    Должны проверять i / i -1
+    Если разные, то arr[insertNums] = arr[i] ;  insertNums++;
+     */
     public static int removeDuplicates(int[] nums) {
         int insertNums = 1;
         for(int i = 1; i < nums.length; i++) {
