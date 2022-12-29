4d5eb0585852b94c52b38947798d006bff0a3dba
OB11TO <OB11TO61@yandex.ru>
2022-12-29 13:43:22 +0300

    [leetcode] -> NumberOf1Bits

---
+++ src/leetcode/easy/NumberOf1Bits.java
@@ -0,0 +1,14 @@
+package leetcode.easy;
+
+public class NumberOf1Bits {
+
+    public static void main(String[] args) {
+
+    }
+
+    // you need to treat n as an unsigned value
+    public static int hammingWeight(int n) {
+       return Integer.bitCount(n);
+    }
+}
+
