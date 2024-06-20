cf87b87caa335a5a48012fedada9565766b61711
Artem Dagridjan <adagridjan@alvioneurope.ru>
2024-06-20 17:55:43 +0300

    169. Majority Element https://leetcode.com/problems/majority-element/description/

--- src/leetcode/easy/MajorElement.java
+++ src/leetcode/easy/MajorElement.java
@@ -27,6 +27,24 @@ public class MajorElement {
         }
         return map.get(nums[0]);
     }
+    public int majorityElement3(int[] nums) {
+        int majority = 0;
+        int result = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+            if(map.get(nums[i]) > majority) {
+                result = nums[i];
+                majority = map.get(nums[i]);
+            }
+        }
+
+        return result;
+    }
 
     public int majorityElement2(int[] nums) {
         HashMap<Integer, Integer> hash = new HashMap<>();
