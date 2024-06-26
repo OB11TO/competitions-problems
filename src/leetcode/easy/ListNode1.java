ed0162ad003677abd3d9006f78006b9058781c62
Artem Dagridjan <adagridjan@alvioneurope.ru>
2024-06-26 12:06:47 +0300

    83. Remove Duplicates from Sorted List https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/

---
+++ src/leetcode/easy/ListNode1.java
@@ -0,0 +1,18 @@
+package leetcode.easy;
+
+public class ListNode1 {
+    int val;
+    ListNode1 next;
+
+    ListNode1() {
+    }
+
+    ListNode1(int val) {
+        this.val = val;
+    }
+
+    ListNode1(int val, ListNode1 next) {
+        this.val = val;
+        this.next = next;
+    }
+}
